16096c642c9695ffd1f76c34a735d721c4bb3860
Lucas <132583860+LucasGanan@users.noreply.github.com>
2026-06-24 10:01:18 -0300

    .

---
+++ Projeto_Final/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const PORT = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// Rotas
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "pages", "index.html"));
+});
+
+app.get("/sobre", (req, res) => {
+    res.sendFile(path.join(__dirname, "pages", "sobre.html"));
+});
+
+app.get("/produtos", (req, res) => {
+    res.sendFile(path.join(__dirname, "pages", "produtos.html"));
+});
+
+app.get("/contato", (req, res) => {
+    res.sendFile(path.join(__dirname, "pages", "contato.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
